6e2ec9eb3b434df497cec1156fb70cf895ea8b66
rahul <rahul352sj@gmail.com>
2024-11-16 15:37:48 +0530

    first

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from './Layout'
+import Slider from './Slider'
 // f4f00a YELLOw
 // 121a23
 
@@ -7,14 +8,16 @@ import Layout from './Layout'
 const Home = () => {
   return (
     <Layout>
-      <div className="md:w-6/12 w-11/12 m-auto">
-      <div className='min-h-[50vh] md:gap-14 gap-8 flex justify-center flex-col items-center'>
+      <div className='md:w-6/12 w-11/12 m-auto min-h-[50vh] md:gap-14 gap-8 flex justify-center flex-col items-center'>
         <h1 className='text-white md:text-4xl text-2xl font-extrabold'>"Transforming Ideas into Visual <span className='md:text-5xl text-3xl text-[#FFD700]'>Masterpieces</span>."</h1>
         <div className="w-full md:space-x-10 space-x-5 md:pl-5">
           <button className='bg-[#FFD700] text-black px-6 py-3 rounded font-semibold'>Hire Me !</button>
           <button className='border border-[#FFD700] text-white px-6 py-3 rounded font-semibold'>Resume !</button>
         </div>
       </div>
+      <div className="md:w-8/12 w-11/12 m-auto">
+        <h1 className='font-bold text-4xl text-[#FFD700] mb-5'>Our Client`s</h1>
+        <Slider />
       </div>
     </Layout>
 
---
+++ src/components/Slider.jsx
@@ -0,0 +1,119 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+// import 'swiper/css/pagination';
+
+// import './styles.css';
+
+// import required modules
+import { Pagination, Autoplay } from 'swiper/modules';
+import { Link } from 'react-router-dom';
+
+
+const Slider = () => {
+
+  const [clients, setClients] = useState([
+    {
+      name : 'clinet',
+      message : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, dolorem.',
+      image : '/images/logo.jpg',
+      link : 'https://youtube.com/@dreamverse777?si=3z6PcV_oVTNN0hjW'
+    },{
+      name : 'clinet',
+      message : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, dolorem.',
+      image : '/images/logo.jpg',
+      link : 'https://youtube.com/@dreamverse777?si=3z6PcV_oVTNN0hjW'
+    },{
+      name : 'clinet',
+      message : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, dolorem.',
+      image : '/images/logo.jpg',
+      link : 'https://youtube.com/@dreamverse777?si=3z6PcV_oVTNN0hjW'
+    },{
+      name : 'clinet',
+      message : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, dolorem.',
+      image : '/images/logo.jpg',
+      link : 'https://youtube.com/@dreamverse777?si=3z6PcV_oVTNN0hjW'
+    },{
+      name : 'clinet',
+      message : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, dolorem.',
+      image : '/images/logo.jpg',
+      link : 'https://youtube.com/@dreamverse777?si=3z6PcV_oVTNN0hjW'
+    },{
+      name : 'clinet',
+      message : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, dolorem.',
+      image : '/images/logo.jpg',
+      link : 'https://youtube.com/@dreamverse777?si=3z6PcV_oVTNN0hjW'
+    },{
+      name : 'clinet',
+      message : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis, dolorem.',
+      image : '/images/logo.jpg',
+      link : 'https://youtube.com/@dreamverse777?si=3z6PcV_oVTNN0hjW'
+    },
+  ])
+  return (
+    <>
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={30}
+      pagination={{
+        clickable: true,
+        
+      }}
+      modules={[Pagination, Autoplay]}
+      className="mySwiper md:flex hidden"
+    >
+      {
+        clients.map((item, index) => (
+          <SwiperSlide key={index} className='md:w-8/12 w-11/12 m-auto flex justify-center items-center flex-col border border-white rounded-lg py-3'>
+            <img src={item.image} alt="" className='h-[150px] w-[150px] object-top object-cover' />
+            <div className="w-[300px] space-y-3 mt-2">
+              <h1 className='text-center text-white'>{item.name}</h1>
+              <p className='text-center text-white'>{item.message}</p>
+              <div className="flex justify-center">
+                <Link to={item.link} target='blank' className='block w-fit bg-[#FFD700] font-semibold px-4 py-1'>
+                  View
+                  <i className="ri-arrow-right-line ml-2"></i>
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))
+      }
+    </Swiper>
+{/* mobile */}
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      pagination={{
+        clickable: true,
+        
+      }}
+      modules={[Pagination, Autoplay]}
+      className="mySwiper md:hidden flex"
+    >
+      {
+        clients.map((item, index) => (
+          <SwiperSlide key={index} className='md:w-8/12 w-11/12 m-auto flex justify-center items-center flex-col border border-white rounded-lg py-3'>
+            <img src={item.image} alt="" className='h-[150px] w-[150px] object-top object-cover' />
+            <div className="w-[300px] space-y-3 mt-2">
+              <h1 className='text-center text-white'>{item.name}</h1>
+              <p className='text-center text-white'>{item.message}</p>
+              <div className="flex justify-center">
+                <Link to={item.link} target='blank' className='block w-fit bg-[#FFD700] font-semibold px-4 py-1'>
+                  View
+                  <i className="ri-arrow-right-line ml-2"></i>
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))
+      }
+    </Swiper>
+  </>
+  )
+}
+
+export default Slider
