f5964c4cf4dcffae982e794441e9d5c7c12dd3f4
rahul <rahul352sj@gmail.com>
2024-11-16 17:22:32 +0530

    first

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
         <div className=" flex justify-center items-center flex-col md:gap-y-9 gap-5">
           <h1 className='text-white md:text-4xl text-2xl font-extrabold w-full'>"Transforming Ideas into Visual </h1>
           <h1 className='text-white md:text-4xl text-2xl font-extrabold w-full'><span className='md:text-5xl text-3xl text-[#FFD700]'>Masterpieces.</span>"</h1>
-          <div className="w-full md:space-x-10 space-x-5 md:pl-5">
+          <div className="w-full md:space-x-10 space-x-5">
             <button className='bg-[#FFD700] text-black px-6 py-3 rounded font-semibold'>Hire Me !</button>
             <button className='border border-[#FFD700] text-white px-6 py-3 rounded font-semibold'>Resume !</button>
           </div>
