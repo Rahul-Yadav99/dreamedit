fdbf62fc786f2b49f04b9b0aeb931d7b0ca6f24e
rahul <rahul352sj@gmail.com>
2024-11-16 18:51:06 +0530

    first

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,8 +9,8 @@ const Home = () => {
   return (
     <Layout>
       <div className='md:w-8/12 w-11/12 m-auto md:min-h-screen min-h-[90vh] grid md:grid-cols-2 grid-cols-1'>
-        <div className="h-full flex justify-center items-center flex-col md:gap-y-9 gap-5">
-          <h1 className='text-white md:text-4xl text-2xl font-extrabold w-full'>"Transforming Ideas into Visual<span className='md:text-5xl text-3xl ml-2 text-[#FFD700]'>Masterpieces</span>."</h1>
+        <div className="h-full flex justify-center items-start flex-col md:gap-y-9 gap-5">
+          <h1 className='text-white md:text-4xl text-2xl font-extrabold'>"Transforming Ideas into Visual <span className='md:text-5xl text-3xl ml-2 text-[#FFD700]'>Masterpieces</span> ." </h1>
           <div className="w-full md:space-x-10 space-x-5">
             <button className='bg-[#FFD700] text-black px-6 py-3 rounded font-semibold'>Hire Me !</button>
             <button className='border border-[#FFD700] text-white px-6 py-3 rounded font-semibold'>Resume !</button>
