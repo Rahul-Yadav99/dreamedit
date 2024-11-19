e562ad610b1d1626c136cd68b9088e69b66201d0
rahul <rahul352sj@gmail.com>
2024-11-19 12:12:28 +0530

    first

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Layout from './Layout'
 import ReactSlider from './ReactSlider'
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap';
 const Home = () => {
+
+  const boxRef = useRef(null);
+  const textRef = useRef(null);
+
+  useGSAP(() => {
+    // Initialize GSAP Timeline
+    const timeline = gsap.timeline();
+
+    // Define animations
+    timeline
+      .from(boxRef.current, {
+        y: 300,
+        duration: 1,
+        delay: 0.5,
+        opacity: 0,
+      })
+      .from(textRef.current, {
+        y: 300,
+        duration: 1,
+        opacity: 0,
+      });
+
+    // Cleanup (optional if animations won't reinitialize)
+    return () => {
+      timeline.kill();
+    };
+  });
+
   return (
     <Layout>
       <div className='md:w-8/12 w-11/12 m-auto md:min-h-screen min-h-[90vh] grid md:grid-cols-2 grid-cols-1'>
-        <div className="h-full flex justify-center items-start flex-col md:gap-y-9 gap-5">
+        <div ref={boxRef} className="heroOne h-full flex justify-center items-start flex-col md:gap-y-9 gap-5">
           <h1 className='text-white md:text-4xl text-2xl font-extrabold'>"Transforming Ideas into Visual <span className='md:text-5xl text-3xl ml-2 text-[#FFD700]'>Masterpieces</span> ." </h1>
           <div className="w-full md:space-x-10 space-x-5">
             <button className='bg-[#FFD700] text-black px-6 py-3 rounded font-semibold'>Hire Me !</button>
@@ -13,7 +43,7 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="h-full flex justify-center items-center">
+        <div ref={textRef} className="heroTwo h-full flex justify-center items-center">
           <img src="/images/hero.svg" alt="" />
         </div>
       </div>
